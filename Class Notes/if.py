4715b427f4650276c0dedb0866ca88dcdfdadf16
minefarmer <pgoldfarm@yahoo.com>
2020-04-19 20:04:28 -0500

    shutting down for a break

---
+++ Class Notes/if.py
@@ -0,0 +1,49 @@
+#  If introductiom\
+# print('enter your age ')
+# age = int(input())
+# if age > 18:
+# 	print('you can enter') # enter your age 
+# 						   # 19
+# 						   # you can enter
+# else:
+# 	print('you cannot enter') # enter your age 
+# 							  # 10 
+# 							  # you cannot enter
+
+# print('enter your age ')
+# age = int(input())  # enter your age 
+# 					# 12
+# 					# you can not enter
+# if age < 18:
+# 	print('you can not enter')
+# elif age > 18 and age <60:
+# 	print('You can enter')
+# else:
+# 	print('you are too old to enter')
+
+# print('enter your age ')
+# age = int(input())  # enter your age 
+# 					# 12
+# 					# you can not enter
+# if age < 18:
+# 	print('you can not enter')
+# elif age > 18 and age <60:
+# 	print('You can enter')
+# else:
+# 	print('you are too old to enter')
+print('enter your age ')
+age = int(input())
+if age < 18:
+	print('you can not enter')  # enter your age 
+			  					# 12
+			  					# you can not enter
+elif age > 18 and age <60:
+	print('You can enter')  # enter your age 
+							# 25
+							# You can enter
+elif age > 60:
+	print('you are too old to enter')  # enter your age 
+									   # 81
+									   # you are too old to enter
+else:
+	print("please enter your")
---
+++ py/main.py
@@ -0,0 +1,78 @@
+# for loop intro
+
+# for i in range(0, 10):
+# 	print('salam')  # salem ** 10 times
+# 	print(i)  # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+# my_list = [1, 2, 3, 4, 5, 6, 7]
+
+# for i in my_list:
+# 	print(i)  # 1 thru 7  ** see below
+# 	print('salem')  # 1
+# 					# salem
+# 					# 2
+# 					# salem
+# 					# 3
+# 					# salem
+# 					# 4
+# 					# salem
+# 					# 5
+# 					# salem
+# 					# 6
+# 					# salem
+# 					# 7
+# 					# salem
+
+
+# for i in range(10):
+# 	print(i)  # 1 thru 7  ** see below
+# 	print('salem')  # 0
+# 					# salem
+# 					# 1
+# 					# salem
+# 					# 2
+# 					# salem
+# 					# 3
+# 					# salem
+# 					# 4
+# 					# salem
+# 					# 5
+# 					# salem
+# 					# 6
+# 					# salem
+# 					# 7
+# 					# salem
+# 					# 8
+# 					# salem
+# 					# 9
+# 					# salem
+
+
+# for i in range(0, 10, 2):
+# 	print(i)
+# 	print('salem')  # 0
+# 					# salem
+# 					# 2
+# 					# salem
+# 					# 4
+# 					# salem
+# 					# 6
+# 					# salem
+# 					# 8
+# 					# salem
+
+
+string = 'salem'
+for i in string:
+	print(i)
+	print('salem')  # s
+					# salem
+					# a
+					# salem
+					# l
+					# salem
+					# e
+					# salem
+					# m
+					# salem
+	
