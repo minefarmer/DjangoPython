72a36a3a58aad28129bdd49f76a02aba68fcf732
minefarmer <pgoldfarm@yahoo.com>
2020-04-30 19:21:27 -0500

    Class Notes/For_add_to_list: createshuting down for the night.

---
+++ Class Notes/For_add_to_list.py
@@ -0,0 +1,55 @@
+l1 =['ali', 'reza', 'pedram', 'mohamad']
+l2 = ['maryam', 'john', 'chris', 'ali', 'pedram']
+l3 = []
+
+# for i in l1:
+#     print(i)    # ali
+#                 # reza
+#                 # pedram
+#                 # mohamad
+
+
+# roundl = 1
+
+# for i in l1:
+#     print(roundl)   # 1
+#     print(i)        # ali
+#                     # 2
+#                     # reza
+#                     # 3
+#                     # pedram
+#                     # 4
+#                     # mohamad
+#     roundl += 1
+
+
+# roundl = 1
+
+# for i in l1:
+#     print(roundl)   # 1
+#     print(i)        # ali
+#                     # 2
+#                     # reza
+#                     # 3
+#                     # pedram
+#                     # 4
+#                     # mohamad
+#     roundl += 1
+
+
+# roundl = 1
+
+# for i in l1:
+#     print('now is', roundl) # now is 1
+#     print(i)                # ali
+#                             # now is 2
+#                             # reza
+#                             # now is 3
+#                             # pedram
+#                             # now is 4
+#                             # mohamad
+#     roundl += 1
+
+
+# roundl = 1
+
