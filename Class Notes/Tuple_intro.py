80d3a5087d588ea44e2d74ade3631ec6181b061d
minefarmer <pgoldfarm@yahoo.com>
2020-05-02 09:20:41 -0500

    bring up to date

---
+++ Class Notes/Tuple_intro.py
@@ -0,0 +1,16 @@
+# a = ();print(type(a))  # <class 'tuple'>
+
+
+users = ("carl", 'matson', 'root', 'administrator', 'public')
+password = ('123', '1234', '12345', '123456')
+# print(users[0])  # carl
+# print(users.index('carl'))  # 0
+
+l = []
+l.append(users);l.append(password)
+# print(l)  # [('carl', 'matson', 'root', 'administrator', 'public'), ('123', '1234', '12345', '123456')]
+# print(l[1])  # ('123', '1234', '12345', '123456')
+# print(l[1]);print(type(l[1]));print(type(l[0]));print('classe 1 hast', type(l))  #('123', '1234', '12345', '123456')
+                                                                                # <class 'tuple'>
+                                                                                # <class 'tuple'>
+                                                                                # classe 1 hast <class 'list
