82ad6422aba691d0765141cd3716888f34c3c046
minefarmer <pgoldfarm@yahoo.com>
2020-06-08 17:54:03 -0500

    python not working right in Win 10

--- Django Python Class Notes/Function Dict.py
+++ Django Python Class Notes/Function Dict.py
@@ -32,13 +32,48 @@
 # print(l1)  # ['h1', 'h2', 'h3', 'h4', 'h5', 's1', 's2', 's3', 's4']
 
 
-# Functio with any number of parameters
-def test(*t):  # *t makes it possible to use any number of perameters
-    print(t)  # ('rich',)  ** ('carl', 'matson')  *** ('carl', 'Richard', 'matson')  **** ('carl', 'Richard', 'matson', '81 years old')
+# # Functio with any number of parameters
+# def test(*t):  # *t makes it possible to use any number of perameters
+#     print(t)  # ('rich',)  ** ('carl', 'matson')  *** ('carl', 'Richard', 'matson')  **** ('carl', 'Richard', 'matson', '81 years old')
     
-test('rich')  # #
-test('carl', 'matson')  # **
-test('carl', 'Richard', 'matson')  # ***
-test('carl', 'Richard', 'matson', '81 years old')  # ****
+# test('rich')  # #
+# test('carl', 'matson')  # **
+# test('carl', 'Richard', 'matson')  # ***
+# test('carl', 'Richard', 'matson', '81 years old')  # ****
+
+
+# def make_pizza(size, *toppings):
+#     print('\n making a ' + str(size) + 'cmm pizza with following toppimgs: ')
+#     for topping in toppings:
+#        print('_ ' + topping.title())  #   making a 16cmm pizza with following toppimgs: 
+#                                         # _ Mushroom
+#                                         # _ Tomato  
+#                                         # _ Cheese  
+#                                         # _ Pepper
+       
+# make_pizza(16, 'mushroom', 'tomato', 'cheese', 'pepper', ) 
+
+
+# def build_profile(first, last, **user_info):
+#     p = {}
+#     p['first_name'] = first
+#     p['last_name'] = last
+#     for k, v in user_info.items():
+#         p[k] = v
+# m = build_profile('carl', 'matson', location='USA', field='miner')
+# print(m)  # None
+
+
+def build_profile(first, last, **user_info):   # continuation of above work to eliminate None print out.  Needed return added
+    p = {}
+    p['first_name'] = first
+    p['last_name'] = last
+    for k, v in user_info.items():
+        p[k] = v
+    return p
+m = build_profile('carl', 'matson', location='paris', field='miner', job='programmer')
+print(m)  # {'first_name': 'carl', 'last_name': 'matson', 'location': 'paris', 'field': 'miner', 'job': 'programmer'}
+
+d = dict(location='shiras', field= 'physics');print(d)  # {'location': 'shiras', 'field': 'physics'}
 
 
