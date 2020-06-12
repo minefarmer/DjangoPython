7a288dc1cc54a4e32775f64396742d49c7734804
minefarmer <pgoldfarm@yahoo.com>
2020-06-12 17:17:08 -0500

    something is not right and can't figure it out, need to sleep on it.

--- Django Python Class Notes/Files.py
+++ Django Python Class Notes/Files.py
@@ -1,3 +1,14 @@
+# FILES WITH OPEN  ** 50
+# BETTER WAY TO DO THE ABOVE WORK  60
+# rstrip  80
+# # # easy way to create a new file  95  ******************************
+# # #   readlines  # 100  
+# class string  # 105
+# #  class list  # 115
+# 
+# 
+# 
+
 # f = open('test.txt')
 # f = f.read()
 # print(f)
@@ -28,8 +39,109 @@
 #                         #     f.write('Hello World!', '\n')
 #                         # TypeError: write() takes exactly one argument (2 given)
 
-f = open('test.txt', 'w')
-f.write('Hello World!\n')
-f.close()
-file_1 = open('test.txt', 'r')
-print(file_1.read())  # Hello World!
+# f = open('test.txt', 'w')
+# f.write('Hello World!\n')
+# f.close()
+# file_1 = open('test.txt', 'r')
+# print(file_1.read())  # Hello World!
+
+
+
+# FILES WITH OPEN  ** 50
+# file = open('rich.txt', 'r')
+# f = file.read();print(f)  # nothing printed == empty file
+
+# file = open('rich.txt', 'w')   # ** Do not use **
+# file.write('hello world\n')
+# file.close()  # must always be done to save the file  *** not workin g as shown in video
+
+
+
+#  BETTER WAY TO DO THE ABOVE WORK  60
+# with open('rich.txt') as file:
+#     f = file.read()  # can use file instead of f
+# print(f)  # hello world
+
+# with open('rich.txt', 'w') as file:
+#     file.write('hi \n helloworld \n bye')  # can use file instead of f
+# print(file)  # <_io.TextIOWrapper name='rich.txt' mode='w' encoding='cp1252'>
+
+
+
+# with open('rich.txt', 'w') as file:
+#     file.write('hi \n hello world \n bye')  # can use file instead of f
+# file.close()
+
+# with open ('rich.txt') as f:
+#     f = f.read()
+# print(f)  # hi
+#             # hello world 
+#             # bye
+# rstrip
+# with open('rich.txt', 'w') as file:
+#     file.write('hi \n hello world \n bye')  # can use file instead of f
+# file.close()
+
+# with open ('rich.txt') as f:
+#     f = f.read()
+# print(f.rstrip())  # hi
+#                     # hello world 
+#                     # bye
+
+
+
+
+
+#  create new file  95
+# with open ('Django Python Class Notes/testA.txt', 'w') as file:
+#     file.write('hello world\n this is python3 \n python3 is the best \n how to open files in python3')
+
+
+#  readlines  # 100
+# with open ('Django Python Class Notes/testA.txt', 'r') as file:
+#     file = file.readlines()
+# print(file)  # ['hello world\n', ' this is python3 \n', ' python3 is the best \n', ' how to open files in python3']
+
+#  class string  # 105
+# filename = 'Django Python Class Notes/carl.txt'
+# with open(filename, 'r') as file:
+#     line = file.readlines()
+    
+#     string = ''
+    
+#     print(type(line))  # <class 'list'>
+  
+    
+# #  class list 
+# filename = 'Django Python Class Notes/carl.txt'
+# with open(filename) as file:
+#     line = file.readlines()
+    
+# string = ''
+    
+# for i in line:
+#     print(i)
+
+
+# filename = 'Django Python Class Notes/carl.txt'
+# with open(filename) as file:
+#     line = file.readlines()
+    
+# string = ''
+    
+# for i in line:
+#     string += i.rstrip()
+    
+#     print(string)  # hello world
+
+
+
+
+# filename = 'Django Python Class Notes/carl.txt'
+with open('carl.txt, 'r' ) as file:
+    file = file.readlines()
+string = ''
+    
+# for i in file:
+#     string += f.rstrip()
+#     print(string)  # hello world
