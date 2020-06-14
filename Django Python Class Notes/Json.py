e586e4c8656fd35cda053a3ba211533a54f0ed41
minefarmer <pgoldfarm@yahoo.com>
2020-06-14 14:58:57 -0500

    nap time

--- Django Python Class Notes/Json.py
+++ Django Python Class Notes/Json.py
@@ -6,9 +6,25 @@ import json
 # 
 # 
 # 
-# n = [1, 2, 3, 4, 5]
-# n.append(60)
-# print(n)  # [1, 2, 3, 4, 5, 60]
+# 
+# 
+
+
+filename = 'username.json'
+
+try:
+    with open('Django Python Class Notes/Json1.py') as file:
+        username = json.load(file)
+except FileNotFoundError:
+    username = input('Whats your username: ')
+    with open('Django Python Class Notes/Json1.py', 'w') as file:
+        json.dump(username, file)
+        print('well remember you when you come back ' + username + ' !')
+
+else:
+    print('welcome back ' + username + ' !')  # welcome back rich !
+
+
 
 
 # num = [2, 3, 4, 5, 6]
@@ -24,14 +40,14 @@ import json
 # print(my_list[1])  # 3
 
 
-username = input('please enter your username: ')  # rich
-filename = 'username.json'
-with open(filename, 'w') as file:
-    json.dump(username, file)
-print('we will remember you when you come back {} !'.format(username))  # we will remember you when you come back rich !
+# username = input('please enter your username: ')  # rich
+# filename = 'username.json'
+# with open(filename, 'w') as file:
+#     json.dump(username, file)
+# print('we will remember you when you come back {} !'.format(username))  # we will remember you when you come back rich !
 
 
-filename = 'username.json'
-with open(filename, 'r') as file:
-    username = json.load(file)
-print(f'welcome back {username}')  # welcome back rich
+# filename = 'username.json'
+# with open(filename, 'r') as file:
+#     username = json.load(file)
+# print(f'welcome back {username}')  # welcome back rich
---
+++ python/main.py
@@ -0,0 +1,3 @@
+print('Hello World!')
+
+# # filename = 'Django Python Class Notes/testA.txt'
