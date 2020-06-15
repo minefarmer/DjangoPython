219e88afcdfcab2de7e0d145edf170c9e2d98120
minefarmer <pgoldfarm@yahoo.com>
2020-06-15 09:43:25 -0500

    "Django Python Class Notes/TestB.py": create nap time

---
+++ Django Python Class Notes/TestB.py
@@ -0,0 +1,36 @@
+# read an existing file in the terminal  11
+# alt read an existing file in the terminal   20
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# f = open('Django Python Class Notes/testA.txt')
+# f = f.read()
+# print(f)  # hello world
+#             # this is python3 
+#             # python3 is the best 
+#             # how to open files in python3
+
+
+
+# #  alt read an existing file in the terminal
+# f = open('Django Python Class Notes/testA.txt')
+# file_1 = f.read()
+# print(file_1)  # hello world
+#             # this is python3 
+#             # python3 is the best 
+#             # how to open files in python3
+
+
+
+#  alt read an existing file in the terminal
+f = open('Django Python Class Notes/testA.txt')
+file_1 = f.read()
+print(file_1)  # hello world
+            # this is python3 
+            # python3 is the best 
+            # how to open files in python3
