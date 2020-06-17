601a2ecc87980a35e52fe340f4bc800778528691
minefarmer <pgoldfarm@yahoo.com>
2020-06-17 14:34:03 -0500

    "Django Python Class Notes: nap time

--- Django Python Class Notes/Argv.py
+++ Django Python Class Notes/Argv.py
@@ -1,3 +1,14 @@
+# not working  21
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+from sys import argv
 # import sys  # importing sys puts a heavy load on the computer and is not recommended
 
     # print(dir(sys))  # ['__breakpointhook__', '__displayhook__', '__doc__', '__excepthook__', '__interactivehook__', '__loader__', '__name__', '__package__', '__spec__', '__stderr__', '__stdin__', '__stdout__', '__unraisablehook__', '_base_executable', '_clear_type_cache', '_current_frames', '_debugmallocstats', '_enablelegacywindowsfsencoding', '_framework', '_getframe', '_git', '_home', '_xoptions', 'addaudithook', 'api_version', 'argv', 'audit', 'base_exec_prefix', 'base_prefix', 'breakpointhook', 'builtin_module_names', 'byteorder', 'call_tracing', 'callstats', 'copyright', 'displayhook', 'dllhandle', 'dont_write_bytecode', 'exc_info', 'excepthook', 'exec_prefix', 'executable', 'exit', 'flags', 'float_info', 'float_repr_style', 'get_asyncgen_hooks', 'get_coroutine_origin_tracking_depth', 'getallocatedblocks', 'getcheckinterval', 'getdefaultencoding', 'getfilesystemencodeerrors', 'getfilesystemencoding', 'getprofile', 'getrecursionlimit', 'getrefcount', 'getsizeof', 'getswitchinterval', 'gettrace', 
@@ -7,6 +18,17 @@
 # print(dir(sys.float_repr_style))  # ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
 
 
+# # not working  21
+# from sys import argv
+
+# source, user_name = argv
+# p = '> '
+# # print(f'hi {user_name}')
+# print(f'I like to ask you a few questions {user_name}')
+# print(f'where do you live {user_name}')
+# like = input(p)
+
+# print(f'so you live in {like}')
 
 
 
@@ -16,15 +38,13 @@
 
 
 
-# /usr/bin/python3 "/home/carl/Github/DjangoPython/Django Python Class Notes/Argv.py"
 
-from sys import argv
+# 
+script, file_name = argv
 
-s, user_name = argv
-p = '> '
-# print(f'hi {user_name}')
-print(f'I like to ask you a few questions {user_name}')
-print(f'where do you live {user_name}')
-like = input(p)
+print(f'so your file is{file_name}')
 
-print()
+f = open(file_name)
+
+print('here is your file name ')
+print(f.read())
