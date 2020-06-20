23ba21a1b2e04e6824467861d2ddd71142a5deeb
minefarmer <pgoldfarm@yahoo.com>
2020-06-20 12:21:42 -0500

    updating changes

--- Django Python Class Notes/Tkinter/TextBox.py
+++ Django Python Class Notes/Tkinter/TextBox.py
@@ -7,10 +7,31 @@ from tkinter import ttk
 # 
 # 
 
+# win = tk.Tk()
+# win.title('Python tkinter ')
 
+# aLabel = ttk.Label(win, text='File name: ')
+# aLabel.grid(column=0, row=0)
 
+# def clickMe():
+#     file_name = name.get()
+#     with open(file_name) as file:
+#         f = file.read()
+        
+#     action.configure(text=f)
+#     aLabel.configure(foreground='green')
 
+# action = ttk.Button(win, text='click me', command=clickMe)
+# action.grid(column=1, row=1)
 
+# name = tk.StringVar()
+
+# textbox = ttk.Entry(win, width=12, textvariable=name)
+# textbox.grid(column=0, row=1)
+
+# win.mainloop()
+
+##########################################
 win = tk.Tk()
 win.title('copy .... past')
 
