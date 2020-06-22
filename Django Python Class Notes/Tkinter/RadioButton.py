e3147f9ea7c03c6d9841fb2317ca76c37a60996a
minefarmer <pgoldfarm@yahoo.com>
2020-06-21 19:34:30 -0500

    domestic time

--- Django Python Class Notes/Tkinter/RadioButton.py
+++ Django Python Class Notes/Tkinter/RadioButton.py
@@ -1,32 +1,16 @@
 import tkinter as tk 
 from tkinter import ttk
 
-# Check Button  29
+# radio button  58
 # 
 # 
 # 
 # 
 
-# win = tk.Tk()
-
-# win.title('python check button')
-
-
-# chvar = tk.IntVar()
-
-# check1 = tk.Checkbutton(win, text='Enable', variable=chvar, state='disabled')
-# check1.select()
-# check1.grid(column=0, row=0)
-# chvar2 = tk.IntVar()
-# check2 = tk.Checkbutton(win, text='Disable', variable=chvar2)
-# check2.deselect()
-# check2.grid(column=1, row=0)
-
-# win.mainloop()
 
 
 
-# Check button
+
 win = tk.Tk()
 
 win.title('python check button')
@@ -72,42 +56,27 @@ action = ttk.Checkbutton(win, text='click me', command=clickMe)
 action.grid(column=2, row=1)
 
 
-win.mainloop()
+color1 = 'Blue'
+color2 = 'Gold'
+color3 = 'Red'
 
 
 
-
-
-
-win = tk.Tk()
-
-win.title('python check button')
-
-# first label
-label = ttk.Label(win, text='Enter a name')
-label.grid(column=0, row=0)
-
-# text Box
-name = tk.StringVar()
-textbox = ttk.Entry(win, width=12, textvariable=name)
-textbox.grid(column=0, row=1)
-
-# check button
-chvar = tk.IntVar()
-chb1 = tk.Checkbutton(win, text='Disable', variable=chvar, state='disabled')
-chb1.grid(column=0, row=2)
-chb1.grid(column=0, row=2, sticky=tk.W)
-
-# second label
-
-label2 = ttk.Label(win, text='Choose a number')
-label2.grid(column=1, row=0)
-
-number = tk.StringVar()
-numberChoose = ttk.Combobox(win, width=12, textvariable=number)
-numberChoose['values'] = (1, 2, 3, 4, 5, 6, 47, 8, 9, 10)
-numberChoose.grid(column=1, row=1)
-numberChoose.current(0)
+def radcall():
+    radsel = radvar.get()
+    if radsel == 1: win.configure(background=color1)
+    elif radsel == 2: win.configure(background=color2)
+    elif radsel == 3: win.configure(background=color3)
+    
+    
+    
+radvar = tk.IntVar()
+rad1 = tk.Radiobutton(win, text=color1, variable=radvar, value=1, command=radcall)
+rad1.grid(column=0, row=5, sticky=tk.W)
+rad2 = tk.Radiobutton(win, text=color2, variable=radvar, value=2, command=radcall)
+rad2.grid(column=1, row=5, sticky=tk.W)
+rad3 = tk.Radiobutton(win, text=color3, variable=radvar, value=3, command=radcall)
+rad3.grid(column=2, row=5)
 
 
 win.mainloop()
